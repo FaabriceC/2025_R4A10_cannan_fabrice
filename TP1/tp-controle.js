0e337254d538da0f1ece422efd31dbe7a910e156
Fabrice95500 <fabricecannan@outlook.com>
2025-02-03 21:12:55 +0100

    TP 1 - JS

--- TP1/tp-controle.js
+++ TP1/tp-controle.js
@@ -8,8 +8,20 @@ module.exports = {
     - Si le nombre est divisible par 3 et par 5, ajouter "Python" au tableau
     - Sinon, ajouter simplement le nombre au tableau
   Renvoyer le tableau final.
-*/
+  */
   F1() {
-
+    const result = [];
+    for (let i = 1; i <= 35; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        result.push("Python");
+      } else if (i % 3 === 0) {
+        result.push("JavaScript");
+      } else if (i % 5 === 0) {
+        result.push("TypeScript");
+      } else {
+        result.push(i);
+      }
+    }
+    return result;
   },
 };
