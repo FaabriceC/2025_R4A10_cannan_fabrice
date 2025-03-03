5516bfdc3f7d2f767b78de3da252e7037ec7c2e4
Fabrice95500 <fabricecannan@outlook.com>
2025-03-03 21:15:36 +0100

    Feat : Add ERROR 404

--- src/app/list-card/list-card.component.ts
+++ src/app/list-card/list-card.component.ts
@@ -7,35 +7,32 @@ import { CardComponent } from '../card/card.component';
   selector: 'app-list-card',
   imports: [CardComponent],
   templateUrl: './list-card.component.html',
-  styleUrls: ['./list-card.component.css'], // Attention à la bonne syntaxe pour styleUrls
+  styleUrls: ['./list-card.component.css'],
 })
 export class ListCardComponent implements OnInit {
   voyages: Voyage[] = [];
-  currentPage = 1; // Page actuelle
-  pageSize = 20; // Nombre de voyages à afficher par page
-  totalPages: number = 1; // Nombre total de pages
+  currentPage = 1;
+  pageSize = 20;
+  totalPages: number = 1;
 
   constructor(private voyageService: VoyageService) {}
 
   ngOnInit(): void {
-    this.voyages = this.voyageService.getVoyages(); // Supposons que getVoyages renvoie tous les voyages
-    this.totalPages = Math.ceil(this.voyages.length / this.pageSize); // Calcul du nombre de pages
+    this.voyages = this.voyageService.getVoyages();
+    this.totalPages = Math.ceil(this.voyages.length / this.pageSize);
   }
 
-  // Retourne les voyages à afficher sur la page courante
   get paginatedVoyages(): Voyage[] {
     const startIndex = (this.currentPage - 1) * this.pageSize;
     return this.voyages.slice(startIndex, startIndex + this.pageSize);
   }
 
-  // Changer la page de pagination
   onPageChange(page: number): void {
     this.currentPage = page;
   }
 
-  // Gérer la suppression d'un voyage
   onVoyageDeleted(id: string): void {
     this.voyages = this.voyages.filter((voyage) => voyage.id !== id);
-    this.totalPages = Math.ceil(this.voyages.length / this.pageSize); // Recalculer le nombre de pages après suppression
+    this.totalPages = Math.ceil(this.voyages.length / this.pageSize);
   }
 }
